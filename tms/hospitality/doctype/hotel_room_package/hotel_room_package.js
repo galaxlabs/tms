5ad8c5a7a874a8a6b4a3e7d10a60b7320e2c114b
galaxlabs <galaxylab2020@gmail.com>
2025-10-24 13:12:46 -0500

    hotels module

---
+++ tms/hospitality/doctype/hotel_room_package/hotel_room_package.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Galaxy Labs and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Hotel Room Package", {
+// 	refresh(frm) {
+
+// 	},
+// });
